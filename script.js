7ddf48be0e042a58879c2a425609c13c19422c59
charankodali <craj19965@gmail.com>
2023-09-28 22:09:24 +0530

    added-3sections

--- script.js
+++ script.js
@@ -15,25 +15,5 @@ mobileMenuButton.addEventListener('click', () => {
 
 
 
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
-    const htmlElement = document.documentElement; // Select the <html> element
-
-    // Function to toggle dark mode
-    function toggleDarkMode() {
-        if (darkModeToggle.checked) {
-            htmlElement.classList.remove('dark'); // Remove 'dark' class to enable light mode
-        } else {
-            htmlElement.classList.add('dark'); // Add 'dark' class to enable dark mode (default)
-        }
-    }
-
-    // Set the initial state of the checkbox based on the default dark mode
-    darkModeToggle.checked = false; // Initially unchecked for light mode
-
-    // Event listener for the dark mode toggle switch
-    darkModeToggle.addEventListener('change', toggleDarkMode);
-
-    // Initial call to set dark mode based on user preference
-    toggleDarkMode();
 
 
