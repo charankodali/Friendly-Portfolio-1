cec1136d7dce9a6f310b4e9b7d0aa1d90c03841f
charankodali <craj19965@gmail.com>
2023-10-01 23:19:51 +0530

    Final-commit

--- script.js
+++ script.js
@@ -7,13 +7,56 @@ mobileMenuButton.addEventListener('click', () => {
     // Toggle the aria-expanded attribute
     mobileMenuButton.setAttribute('aria-expanded', !expanded);
 
-    // Toggle the visibility of the navigation menu
+    // Toggle the hidden attribute on the navbar
     navbarSticky.classList.toggle('hidden');
 });
 
 
 
+//Dark-Mode
+
+const sunIcon = document.querySelector('.sun-icon');
+const moonIcon = document.querySelector('.moon-icon');
+
+
+const userTheme = localStorage.getItem('theme');
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const iconToggle = () => {
+    sunIcon.classList.toggle('display-none');
+    moonIcon.classList.toggle('display-none');
+};
+
+
+const themeCheck = () =>{
+    if(userTheme === 'dark' || (!userTheme && systemTheme)){
+        document.documentElement.classList.add('dark');
+        moonIcon.classList.add("display-none");
+        return;
+}
+sunIcon.classList.add("display-none");
+};
 
 
 
+const themeSwitch = () => {
+    if(document.documentElement.classList.contains('dark')){
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        iconToggle();
+        return;
+    }
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'light');
+    iconToggle();
+};
 
+sunIcon.addEventListener('click',() => {
+    themeSwitch();
+});
+
+moonIcon.addEventListener('click',() => {
+    themeSwitch();
+});
+
+themeCheck();
